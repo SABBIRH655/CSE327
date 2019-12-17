64b4c2d0b57b6428d7914330e03dd4197ef0e1e3
Subir Dey <subir.raju@northsouth.edu>
2019-12-17 19:50:10 +0600

    project updated

---
+++ Web/resources/views/pages/about.blade.php
@@ -0,0 +1,12 @@
+
+
+    @extends('layouts.app')
+    
+    @section('content')
+    <div class="jumbotron text-center">
+        <h1>This is About!</h1>
+        <p>This page is about the developers</p>
+    </div>
+        
+    @endsection
+
