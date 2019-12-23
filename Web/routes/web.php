c825533ed2a6ebe1ce1065ad63fe02b65a4967d9
Subir Dey <subir.raju@northsouth.edu>
2019-12-23 14:05:34 +0600

    Login/Register confirm with database

--- Web/routes/web.php
+++ Web/routes/web.php
@@ -23,3 +23,7 @@ Route::get('/', 'PagesController@index' );
 Route::get('/about', 'PagesController@about' );
 Route::get('/services', 'PagesController@services' );
 Route::resource('post','PostController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
